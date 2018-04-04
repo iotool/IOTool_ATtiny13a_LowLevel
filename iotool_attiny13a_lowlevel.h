a1d8e008c77bb70820398d137c566ef246e4d494
iotool <roha.mbox@gmail.com>
2018-04-05 00:14:44 +0200

    timer0 micros interval

--- iotool_attiny13a_lowlevel.h
+++ iotool_attiny13a_lowlevel.h
@@ -11,6 +11,7 @@
 // 2017-04-07  RoHa  v1.0 read/write pins, deep sleep
 // 2017-04-24  RoHa  v1.1 pwm
 // 2017-11-13  RoHa  v1.2 pb5/adc0
+// 2018-04-04  RoHa  v1.3 timer, toggle
 
 #ifndef IOTOOL_ATTINY13A_LOWLEVEL_H_
 #define IOTOOL_ATTINY13A_LOWLEVEL_H_
@@ -23,23 +24,31 @@
 #include <util/delay.h>      // low level delay
 
 // interrupt timer
-#define SLEEPTIMER_16MS     WDTCR |= (0<<WDP3)|(0<<WDP2)|(0<<WDP1)|(0<<WDP0)
-#define SLEEPTIMER_32MS     WDTCR |= (0<<WDP3)|(0<<WDP2)|(0<<WDP1)|(1<<WDP0)
-#define SLEEPTIMER_64MS     WDTCR |= (0<<WDP3)|(0<<WDP2)|(1<<WDP1)|(0<<WDP0)
-#define SLEEPTIMER_125MS    WDTCR |= (0<<WDP3)|(0<<WDP2)|(1<<WDP1)|(1<<WDP0)
-#define SLEEPTIMER_250MS    WDTCR |= (0<<WDP3)|(1<<WDP2)|(0<<WDP1)|(0<<WDP0)
-#define SLEEPTIMER_500MS    WDTCR |= (0<<WDP3)|(1<<WDP2)|(0<<WDP1)|(1<<WDP0)
-#define SLEEPTIMER_1S       WDTCR |= (0<<WDP3)|(1<<WDP2)|(1<<WDP1)|(0<<WDP0)
-#define SLEEPTIMER_2S       WDTCR |= (0<<WDP3)|(1<<WDP2)|(1<<WDP1)|(1<<WDP0)
-#define SLEEPTIMER_4S       WDTCR |= (1<<WDP3)|(0<<WDP2)|(0<<WDP1)|(0<<WDP0)
-#define SLEEPTIMER_8S       WDTCR |= (1<<WDP3)|(0<<WDP2)|(0<<WDP1)|(1<<WDP0)
-#define SLEEPTIMER_START    WDTCR |= (1<<WDTIE); WDTCR |= (0<<WDE); sei(); set_sleep_mode(SLEEP_MODE_PWR_DOWN)
-#define SLEEPTIMER_SLEEP    sleep_mode()
-#define TIMER_CLEAR         cli()
-#define TIMER_START         sei()
-#define TIMER_USE_OVERFLOW  TIMSK0 |= (1<<TOIE0) /* TIMSK0 |= _BV(TOIE0) */
-#define TIMER_ON_WAKEUP     ISR(WDT_vect)
-#define TIMER_ON_OVERFLOW   ISR(TIM0_OVF_vect)
+#define SLEEPTIMER_16MS      WDTCR |= (0<<WDP3)|(0<<WDP2)|(0<<WDP1)|(0<<WDP0)
+#define SLEEPTIMER_32MS      WDTCR |= (0<<WDP3)|(0<<WDP2)|(0<<WDP1)|(1<<WDP0)
+#define SLEEPTIMER_64MS      WDTCR |= (0<<WDP3)|(0<<WDP2)|(1<<WDP1)|(0<<WDP0)
+#define SLEEPTIMER_125MS     WDTCR |= (0<<WDP3)|(0<<WDP2)|(1<<WDP1)|(1<<WDP0)
+#define SLEEPTIMER_250MS     WDTCR |= (0<<WDP3)|(1<<WDP2)|(0<<WDP1)|(0<<WDP0)
+#define SLEEPTIMER_500MS     WDTCR |= (0<<WDP3)|(1<<WDP2)|(0<<WDP1)|(1<<WDP0)
+#define SLEEPTIMER_1S        WDTCR |= (0<<WDP3)|(1<<WDP2)|(1<<WDP1)|(0<<WDP0)
+#define SLEEPTIMER_2S        WDTCR |= (0<<WDP3)|(1<<WDP2)|(1<<WDP1)|(1<<WDP0)
+#define SLEEPTIMER_4S        WDTCR |= (1<<WDP3)|(0<<WDP2)|(0<<WDP1)|(0<<WDP0)
+#define SLEEPTIMER_8S        WDTCR |= (1<<WDP3)|(0<<WDP2)|(0<<WDP1)|(1<<WDP0)
+#define SLEEPTIMER_START     WDTCR |= (1<<WDTIE); WDTCR |= (0<<WDE); sei(); set_sleep_mode(SLEEP_MODE_PWR_DOWN)
+#define SLEEPTIMER_SLEEP     sleep_mode()
+#define OVERFLOWTIMER_213US  TCCR0B |= (1<<CS00) /* 256x 0.833us @ 1.2Mhz */ 
+#define OVERFLOWCOUNT_25PCT  TCNT0 = 63          /* 160.00 us overflow interval */
+#define OVERFLOWCOUNT_50PCT  TCNT0 = 127         /* 106.66 us overflow interval */
+#define OVERFLOWCOUNT_75PCT  TCNT0 = 191         /*  53.33 us overflow interval */
+#define OVERFLOWCOUNT_10US   TCNT0 = 12          /*  10.00 us overflow interval */
+#define OVERFLOWCOUNT_13US   TCNT0 = 16          /*  13.33 us overflow interval */
+#define OVERFLOWCOUNT_100US  TCNT0 = 120         /* 100.00 us overflow interval */
+#define OVERFLOWCOUNT_125US  TCNT0 = 150         /* 125.00 us overflow interval */
+#define TIMER_CLEAR          cli()
+#define TIMER_START          sei()
+#define TIMER_USE_OVERFLOW   TIMSK0 |= (1<<TOIE0) /* TIMSK0 |= _BV(TOIE0) */
+#define TIMER_ON_WAKEUP      ISR(WDT_vect)
+#define TIMER_ON_OVERFLOW    ISR(TIM0_OVF_vect)
 
 // delay
 #define DELAY_1MS    _delay_ms(1)
@@ -66,18 +75,24 @@
 #define PINMODE_PB5_OUTPUT  DDRB |= (1 << PB5)
 
 // digitalWrite(PBx,LOW/HIGH)
-#define DIGITALWRITE_PB0_LOW   PORTB &= ~(1 << PB0)
-#define DIGITALWRITE_PB1_LOW   PORTB &= ~(1 << PB1)
-#define DIGITALWRITE_PB2_LOW   PORTB &= ~(1 << PB2)
-#define DIGITALWRITE_PB3_LOW   PORTB &= ~(1 << PB3)
-#define DIGITALWRITE_PB4_LOW   PORTB &= ~(1 << PB4)
-#define DIGITALWRITE_PB5_LOW   PORTB &= ~(1 << PB5)
-#define DIGITALWRITE_PB0_HIGH  PORTB |= (1 << PB0)
-#define DIGITALWRITE_PB1_HIGH  PORTB |= (1 << PB1)
-#define DIGITALWRITE_PB2_HIGH  PORTB |= (1 << PB2)
-#define DIGITALWRITE_PB3_HIGH  PORTB |= (1 << PB3)
-#define DIGITALWRITE_PB4_HIGH  PORTB |= (1 << PB4)
-#define DIGITALWRITE_PB5_HIGH  PORTB |= (1 << PB5)
+#define DIGITALWRITE_PB0_LOW     PORTB &= ~(1 << PB0)
+#define DIGITALWRITE_PB1_LOW     PORTB &= ~(1 << PB1)
+#define DIGITALWRITE_PB2_LOW     PORTB &= ~(1 << PB2)
+#define DIGITALWRITE_PB3_LOW     PORTB &= ~(1 << PB3)
+#define DIGITALWRITE_PB4_LOW     PORTB &= ~(1 << PB4)
+#define DIGITALWRITE_PB5_LOW     PORTB &= ~(1 << PB5)
+#define DIGITALWRITE_PB0_HIGH    PORTB |= (1 << PB0)
+#define DIGITALWRITE_PB1_HIGH    PORTB |= (1 << PB1)
+#define DIGITALWRITE_PB2_HIGH    PORTB |= (1 << PB2)
+#define DIGITALWRITE_PB3_HIGH    PORTB |= (1 << PB3)
+#define DIGITALWRITE_PB4_HIGH    PORTB |= (1 << PB4)
+#define DIGITALWRITE_PB5_HIGH    PORTB |= (1 << PB5)
+#define DIGITALWRITE_PB0_TOGGLE  PORTB ^= (1 << PB0)
+#define DIGITALWRITE_PB1_TOGGLE  PORTB ^= (1 << PB1)
+#define DIGITALWRITE_PB2_TOGGLE  PORTB ^= (1 << PB2)
+#define DIGITALWRITE_PB3_TOGGLE  PORTB ^= (1 << PB3)
+#define DIGITALWRITE_PB4_TOGGLE  PORTB ^= (1 << PB4)
+#define DIGITALWRITE_PB5_TOGGLE  PORTB ^= (1 << PB5)
 
 // digitalRead(PBx) == LOW
 #define DIGITALREAD_PB0_LOW   (PINB & (1 << PB0))
@@ -134,4 +149,3 @@ void writePWM1(int val)
 }
 
 #endif // IOTOOL_ATTINY13A_LOWLEVEL_H_
-
